772e2d6ba542f8171b7c2db48bd10801a3126e4b
Abdul Waris <abdulwarisaw532@gmail.com>
2024-01-19 21:00:10 -0500

    Add user input validation on user signup backend endpoint

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -2,15 +2,32 @@
 const express = require('express');
 const { Op } = require('sequelize');
 const bcrypt = require('bcryptjs');
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
 
 const { setTokenCookie, restoreUser } = require('../../utils/auth');
 const { User } = require('../../db/models');
+const { handle } = require('express/lib/router');
 
 const router = express.Router();
+
+
+ // validating login request body
+ const validateLogin = [
+    check('credential')
+      .exists({ checkFalsy: true })
+      .notEmpty()
+      .withMessage('Please provide a valid email or username.'),
+    check('password')
+      .exists({ checkFalsy: true })
+      .withMessage('Please provide a password.'),
+    handleValidationErrors
+  ];
 
 // Log in
 router.post(
     '/',
+    validateLogin,
     async (req, res, next) => {
       const { credential, password } = req.body;
   
@@ -71,5 +88,8 @@ router.post(
       } else return res.json({ user: null });
     }
   );
+
+ 
+  
 
 module.exports = router;
