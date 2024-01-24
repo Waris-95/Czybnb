62fa1ecf69f69c7c335cc7f7470c7003289fab4a
Abdul Waris <abdulwarisaw532@gmail.com>
2024-01-23 20:03:12 -0500

    working db

--- backend/db/migrations/20240120222459-create-spot-image.js
+++ backend/db/migrations/20240120222459-create-spot-image.js
@@ -1,55 +1,55 @@
-'use strict';
+"use strict";
 /** @type {import('sequelize-cli').Migration} */
+
+let options = {};
+if (process.env.NODE_ENV === "production") {
+    options.schema = process.env.SCHEMA;
+}
+
 module.exports = {
-  async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('SpotImages', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      id: {
-        type: Sequelize.INTEGER
-      },
-      ownerId: {
-        type: Sequelize.INTEGER
-      },
-      address: {
-        type: Sequelize.STRING
-      },
-      city: {
-        type: Sequelize.STRING
-      },
-      state: {
-        type: Sequelize.STRING
-      },
-      lat: {
-        type: Sequelize.DECIMAL
-      },
-      lng: {
-        type: Sequelize.DECIMAL
-      },
-      name: {
-        type: Sequelize.STRING
-      },
-      description: {
-        type: Sequelize.STRING
-      },
-      price: {
-        type: Sequelize.DECIMAL
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
-  },
-  async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('SpotImages');
-  }
+    async up(queryInterface, Sequelize) {
+        await queryInterface.createTable(
+            "SpotImages",
+            {
+                id: {
+                    allowNull: false,
+                    autoIncrement: true,
+                    primaryKey: true,
+                    type: Sequelize.INTEGER,
+                },
+                spotId: {
+                    type: Sequelize.INTEGER,
+                    allowNull: false,
+                    references: {
+                        model: "Spots",
+                        key: "id",
+                    },
+                    onDelete: "CASCADE",
+                },
+                url: {
+                    type: Sequelize.STRING,
+                    allowNull: false,
+                },
+                preview: {
+                    type: Sequelize.BOOLEAN,
+                    allowNull: false,
+                },
+                createdAt: {
+                    allowNull: false,
+                    type: Sequelize.DATE,
+                    defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+                },
+                updatedAt: {
+                    allowNull: false,
+                    type: Sequelize.DATE,
+                    defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+                },
+            },
+            options
+        );
+    },
+    async down(queryInterface, Sequelize) {
+        options.tableName = "SpotImages";
+        await queryInterface.dropTable("SpotImages");
+    },
 };
