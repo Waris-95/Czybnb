47dbaf86da65e57740ab89a47ca09c509b0c956c
Abdul Waris <maiyaleeg223@gmail.com>
2024-04-03 17:27:37 -0400

    AuthMe progress

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,22 +1,25 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 import { Provider } from 'react-redux';
 import configureStore from './store';
+import { restoreCSRF, csrfFetch } from './store/csrf';
 
 const store = configureStore();
 
 if (process.env.NODE_ENV !== 'production') {
+  restoreCSRF();
+  
+  window.csrfFetch = csrfFetch;
   window.store = store;
 }
 
-import { createRoot } from 'react-dom/client'; // Import createRoot from react-dom/client
-
-createRoot(document.getElementById('root')).render( // Use createRoot from react-dom/client
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
   </React.StrictMode>
 );
+
