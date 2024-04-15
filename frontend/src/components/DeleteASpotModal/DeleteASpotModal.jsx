fb6c91faa8bc3c0fd687325f9dca83cb15912163
Abdul Waris <maiyaleeg223@gmail.com>
2024-04-15 19:05:15 -0400

    DeleteAReviewModal is working

--- frontend/src/components/DeleteASpotModal/DeleteASpotModal.jsx
+++ frontend/src/components/DeleteASpotModal/DeleteASpotModal.jsx
@@ -1,35 +1,37 @@
-import "./DeleteASpot.css";
-import { useModal } from '../../context/Modal'
 import { useDispatch } from 'react-redux';
-import { deleteASpotThunk } from '../../store/spots'
+import { deleteASpotThunk } from '../../store/spots';
+import { useModal } from '../../context/Modal';
 
-import { useNavigate } from "react-router-dom";
-function DeleteASpotModal({ spot }) {
-    const navigate = useNavigate()
-    const dispatch = useDispatch();
-    const { closeModal } = useModal(); 
+function DeleteASpotModal({ spot, onDeleteSuccess }) {
+  const dispatch = useDispatch();
+  const { closeModal } = useModal();
 
-    const deleteSpot = (e) => {
-        e.preventDefault();
+  const deleteSpot = async (e) => {
+    e.preventDefault();
 
-        dispatch(deleteASpotThunk(spot.id).then(closeModal));
-        navigate('/spots/current');
-    };
+    try {
+      await dispatch(deleteASpotThunk(spot.id));
+      onDeleteSuccess(); // Call the callback function provided by the parent
+    } catch (error) {
+      console.error('Error deleting spot:', error);
+    }
+  };
 
   return (
     <div className="delete-spot-modal">
-        <h1>confirm Delete</h1>
-        <span id="delete-spot-span">
-            <p>Are you sure you want to delete this spot?</p>
-        </span>
-        <button className="confirm-delete" style={{ cursor: "pointer"}}onClick={deleteSpot}>
-            Yes (Delete Spot)
-        </button>
-        <button className="do-not-delete" style={{ cursor: "pointer"}}onClick={closeModal}>
-            No (Keep Spot)
-        </button>
+      <h1>Confirm Delete</h1>
+      <span id="delete-spot-span">
+        <p>Are you sure you want to delete this spot?</p>
+      </span>
+      <button className="confirm-delete" onClick={deleteSpot}>
+        Yes (Delete Spot)
+      </button>
+      <button className="do-not-delete" onClick={closeModal}>
+        No (Keep Spot)
+      </button>
     </div>
-  )
+  );
 }
 
-export default DeleteASpotModal
+export default DeleteASpotModal;
+
