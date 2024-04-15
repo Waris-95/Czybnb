fc721205c136bb56181ac25ab6eeb0fdc4b714c1
Abdul Waris <maiyaleeg223@gmail.com>
2024-04-15 15:39:26 -0400

    Adding for debug

--- frontend/src/components/AllSpot/AllSpot.jsx
+++ frontend/src/components/AllSpot/AllSpot.jsx
@@ -1,31 +1,51 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getAllSpotsThunk } from "../../store/spots";
-import SpotTile from '../SpotTile/SpotTile'
-import "./AllSpot.css"
+import SpotTile from '../SpotTile/SpotTile';
+import "./AllSpot.css";
+import SearchBox from "../Navigation/SearchBox/SearchBox";
 
 function AllSpots() {
-    const dispatch = useDispatch();
-    const spotsObj = useSelector(state => state.spots);
-    
-    // Check if spotsObj is truthy before accessing its values
-    const spots = spotsObj ? Object.values(spotsObj) : [];
+	const [searchField, setSearchField] = useState("");
+	const [filteredSpots, setFilteredSpots] = useState([]);
 
-    useEffect(() => {
-        dispatch(getAllSpotsThunk());
-    }, [dispatch])
+	const dispatch = useDispatch();
+	const spotsObj = useSelector((state) => state.spots);
+	const spots = spotsObj ? Object.values(spotsObj) : [];
 
-    if (!spots.length) return null
+	useEffect(() => {
+		dispatch(getAllSpotsThunk());
+	}, [dispatch]);
 
-    return (
-        <div className="all-spots-container">
-            {spots.map((spot) => (
-                <div className="single-spot-tile" key={spot.id}>
-                    <SpotTile spot={spot} />
-                </div>
-            ))}
-        </div>
-    )
+	useEffect(() => {
+		const newFilteredSpots = spots.filter((spot) => {
+			return spot.name.toLowerCase().includes(searchField.toLowerCase());
+		});
+		setFilteredSpots(newFilteredSpots);
+	}, [spots, searchField]);
+
+	const onSearchChange = (event) => {
+		setSearchField(event.target.value);
+	};
+
+	if (!spots.length) {
+		return null;
+	}
+
+	return (
+		<div className="all-spots-container">
+			<SearchBox
+				className="search-box"
+				onChangeHandler={onSearchChange}
+				placeholder="Search Spots"
+			/>
+			{filteredSpots.map((spot) => (
+				<div className="single-spot-tile" key={spot.id}>
+					<SpotTile spot={spot} />
+				</div>
+			))}
+		</div>
+	);
 }
 
 export default AllSpots;
