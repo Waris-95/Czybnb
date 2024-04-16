52e5fa48c0c804fb7a3eb11a97ecd3ae9ec3aa2a
Abdul Waris <maiyaleeg223@gmail.com>
2024-04-16 13:11:51 -0400

    making the profile button bigger

--- frontend/src/components/Navigation/Navigation.jsx
+++ frontend/src/components/Navigation/Navigation.jsx
@@ -11,8 +11,7 @@ function Navigation({ isLoaded }) {
     <ul className='nav'>
       <div className='snorebnb'>
         <li>
-        <NavLink to="/" style={{ textDecoration: "none" }}>
-
+          <NavLink to="/" style={{ textDecoration: "none" }}>
             <div className='logo-name'>
               <img src="https://th.bing.com/th/id/R.b7f80dcd01c55a674190e186889c21ac?rik=EG4su3o873VEfA&riu=http%3a%2f%2fwww.clker.com%2fcliparts%2fb%2fK%2f7%2fU%2fh%2fz%2fred-house-hi.png&ehk=CG96yp5ZOvVdB0Oyi2AEvY%2f3A1z1gWYiGhOw5zpWPFM%3d&risl=&pid=ImgRaw&r=0&sres=1&sresct=1" alt="logo" style={{ height: '28px', marginRight: '10px' }} />
               <span>czybnb</span>
@@ -22,6 +21,11 @@ function Navigation({ isLoaded }) {
       </div>
       {isLoaded && (
         <>
+          <div className='profilelogo'>
+            <li>
+              <ProfileButton user={sessionUser} />
+            </li>
+          </div>
           {sessionUser && (
             <div className='createspot'>
               <li className='create-spot-container-main'>
@@ -29,11 +33,6 @@ function Navigation({ isLoaded }) {
               </li>
             </div>
           )}
-          <div className='profilelogo'>
-            <li>
-              <ProfileButton user={sessionUser} />
-            </li>
-          </div>
         </>
       )}
     </ul>
