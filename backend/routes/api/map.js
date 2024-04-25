36ab0b2cd3da337ea30ec99f009ab17a6406a0d5
Abdul Waris <maiyaleeg223@gmail.com>
2024-04-24 21:10:51 -0400

    debug

--- backend/routes/api/map.js
+++ backend/routes/api/map.js
@@ -1,10 +1,10 @@
 const router = require('express').Router();
-const googleMapsAPIKey = require('../../config');
+const { googleMapsAPIKey } = require('../../config');
 
 router.post('/key', (req, res) => {
     console.log(googleMapsAPIKey)
     console.log('googleMapsAPIKey', googleMapsAPIKey);
-    res.json({ googleMapsAPIKey: googleMapsAPIKey.jwtConfig.googleMapsAPIKey });
+    res.json({ googleMapsAPIKey: googleMapsAPIKey});
 });
 
 module.exports = router;
