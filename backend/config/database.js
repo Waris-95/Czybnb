da3c28b846eb2886cd94b0116fb2f661a61db21f
Abdul Waris <maiyaleeg223@gmail.com>
2024-07-11 17:52:41 -0400

    Ensure correct production environment settings

--- backend/config/database.js
+++ backend/config/database.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 console.log('Environment:', process.env.NODE_ENV);
 console.log('Database URL:', process.env.DATABASE_URL);
 console.log('Schema:', process.env.SCHEMA);
+console.log('JWT_SECRET:', process.env.JWT_SECRET);
 
 module.exports = {
   development: {
